44002db90e2f6fbaa45654cf56137a8a179f7635
Marc EYMARD <m.eymard@mgo-consulting.com>
2017-11-23 14:15:30 +0100

    Use abstract model

--- api/front/src/Devis.js
+++ api/front/src/Devis.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 
-class Devis extends Component {
+class AbstractModel extends Component {
     constructor(props) {
         super(props);
-        Axios.get('http://localhost:8181/app_dev.php/devis')
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        this.basePath = 'http://localhost:8181/app_dev.php/';
     }
-  render() {
-    return (
-      <div className="Devis">
-        <div className="Devis-header">
-          <h2>Devis</h2>
-        </div>
-      </div>
-    );
-  }
+    getElements() {
+        Axios.get(this.basePath + 'devis')
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+class Devis extends AbstractModel {
+    render() {
+        this.getElements();
+
+        return (
+            <div className="Devis">
+                <div className="Devis-header">
+                    <h2>Devis</h2>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Devis;
